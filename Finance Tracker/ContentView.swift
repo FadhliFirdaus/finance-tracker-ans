432a84658a497027c97d0b5bbbf10cf461e3b1a2
FadhliFirdaus <44931086+FadhliFirdaus@users.noreply.github.com>
2026-05-11 23:42:43 +0800

    Initial Commit

---
+++ Finance Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Finance Tracker
+//
+//  Created by Fadhli Firdaus on 11/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Finance Tracker/Finance_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_TrackerApp.swift
+//  Finance Tracker
+//
+//  Created by Fadhli Firdaus on 11/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct Finance_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
